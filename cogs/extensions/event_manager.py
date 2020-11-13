8974d972316b70111db564d35af59e2263710378
alex <alexandruturcanu915@gmail.com>
2020-11-13 22:36:44 +0300

    Added several more commands to add, edit and remove both Events and Triggers.

--- cogs/extensions/event_manager.py
+++ cogs/extensions/event_manager.py
@@ -1,14 +1,17 @@
-from discord.ext import commands, tasks
-from discord import Embed, Message
+import asyncio
+import copy
+from datetime import datetime, timedelta
 from enum import Enum
-from time import strptime, localtime, struct_time, mktime, strftime
-from datetime import datetime
+from time import strptime, mktime, strftime, asctime
+
+from discord import Embed, Message
+from discord.ext import commands, tasks
 
 import utils
+from objects import EventConfig, Event, EventReminderTrigger
 from objects.configuration import ConfigurationDictionary, Configuration
 from utils.cog_class import CogClass
-
-from objects import EventConfig, Event, EventReminderTrigger
+from utils.dialog_utils import yes_no, DialogReturn
 
 
 class States(Enum):
@@ -65,21 +68,21 @@ class EventManager(CogClass, name=utils.CogNames.EventManager.value):
             if dif > len(guild.events):
                 self.save_configs(guild_id)
 
-    @commands.command()
-    async def get_event_details(self, ctx: commands.Context, index: int = -1):
-        guild: EventConfig = self.guildDict[ctx.guild.id]
+    # region Events
+    @commands.group(name="Event", invoke_without_command=True)
+    async def event(self, ctx: commands.Context, index: int = None):
+        config: EventConfig = self.guildDict[ctx.guild.id]
 
-        if index <= -1:
+        if not index:
             embed: Embed = Embed(title="List of Upcoming Events.")
-            for i in range(len(guild.events)):
-                embed.add_field(name=f"Index {i}:", value=guild.events[i].name, inline=False)
+            for i in range(len(config.events)):
+                embed.add_field(name=f"Index {i + 1}:", value=config.events[i].name, inline=False)
             await ctx.send(embed=embed)
         else:
-            if index > len(guild.events):
-                await ctx.send("Index is outside the range of events.")
-                return
+            if index - 1 >= len(config.events):
+                raise commands.BadArgument(f"Index {index} out of range.")
 
-            event: Event = guild.events[index]
+            event: Event = config.events[index - 1]
             embed = Embed(title=event.name, description=f"{event.description}")
 
             embed.add_field(name="Date:", value=f"{strftime('%A %b %d', event.datetime)}", inline=False)
@@ -112,8 +115,7 @@ class EventManager(CogClass, name=utils.CogNames.EventManager.value):
         for line in content:
             if (hold := line.replace(" ", "")).startswith("[") and hold.endswith("]"):
                 if (guild.name_tag.replace(" ", ""), guild.description_tag.replace(" ", ""),
-                        guild.datetime_tag.replace(" ", "")).__contains__(hold[1:-1]):
-
+                    guild.datetime_tag.replace(" ", "")).__contains__(hold[1:-1]):
                     state = {
                         guild.name_tag.replace(" ", ""): States.NAME,
                         guild.description_tag.replace(" ", ""): States.DESCRIPTION,
@@ -142,6 +144,215 @@ class EventManager(CogClass, name=utils.CogNames.EventManager.value):
 
         guild.events.append(event)
         self.save_configs(message.guild.id)
+
+    @event.command(name="cancel")
+    async def cancel_event(self, ctx: commands.Context, index: int = None, confirm: bool = False):
+        config: EventConfig = self.guildDict[ctx.guild.id]
+        if index - 1 >= len(config.events) or index <= 0:
+            raise commands.BadArgument(f"Index {index} out of range.")
+
+        if confirm:
+            config.events.pop(index - 1)
+            self.save_configs(ctx.guild.id)
+        else:
+            result: DialogReturn = await yes_no(ctx, "Are you sure?",
+                                                f"Are you sure you want to cancel the event {config.events[index - 1].name}?",
+                                                timeout=20.0)
+            if result == DialogReturn.YES:
+                config.events.pop(index - 1)
+                self.save_configs(ctx.guild.id)
+
+        await ctx.send("Done.")
+
+    @event.command(name="edit")
+    async def edit_event(self, ctx: commands.Context, index: int, name: str = "", time: str = "", *,
+                         description: str = ""):
+        config: EventConfig = self.guildDict[ctx.guild.id]
+        if index - 1 >= len(config.events) or index <= 0:
+            raise commands.BadArgument(f"Index {index} out of range.")
+
+        old_event: Event = copy.deepcopy(new_event := config.events[index - 1])
+
+        if name or time or description:
+            if name:
+                new_event.name = name
+            if description:
+                new_event.description = description
+            if time:
+                try:
+                    new_event.datetime = strptime(time)
+                except ValueError:
+                    raise commands.BadArgument(f"Argument '**{time}**' is not in the correct time format.")
+        else:
+            _one = "1\N{COMBINING ENCLOSING KEYCAP}"
+            _two = "2\N{COMBINING ENCLOSING KEYCAP}"
+            _three = "3\N{COMBINING ENCLOSING KEYCAP}"
+            _cancel = "\N{Black Square For Stop}"
+
+            wizard_embed: Embed = Embed()
+            while True:
+                wizard_embed.title = "Edit Event Wizard."
+                wizard_embed.description = "Hello this is an automatic wizard that allows for simple edit to an event. " \
+                                           "It is best to have the changes to the event prepared in advance as you will have 60 seconds to commit you change. " \
+                                           "React with what you want to change. Use the footer as a key."
+
+                wizard_embed.set_footer(text=f"1: Name, 2: Time&Date, 3: Description, Stop: Cancel")
+                wizard_message: Message = await ctx.send(embed=wizard_embed)
+                await wizard_message.add_reaction(_one)
+                await wizard_message.add_reaction(_two)
+                await wizard_message.add_reaction(_three)
+                await wizard_message.add_reaction(_cancel)
+                await asyncio.sleep(1)
+
+                try:
+                    reaction, member = await self.client.wait_for("reaction_add", timeout=60, check=lambda _reaction,
+                                                                                                           _member: _member == ctx.author and _reaction.emoji in (
+                        _one, _two, _three, _cancel))
+                except asyncio.TimeoutError:
+                    await wizard_message.clear_reactions()
+                    return
+                else:
+                    await wizard_message.clear_reactions()
+
+                    def check(_message):
+                        return _message.author == ctx.author
+
+                    wizard_embed.set_footer(text="")
+                    if reaction.emoji == _one:
+                        wizard_embed.title = "Edit Event Name"
+                        wizard_embed.description = "Your next message will be the new name for the event."
+                        await wizard_message.edit(embed=wizard_embed)
+                        try:
+                            user_message = await self.client.wait_for("message", timeout=60, check=check)
+                        except asyncio.TimeoutError:
+                            pass
+                        else:
+                            new_event.name = user_message.content
+                            break
+                    elif reaction.emoji == _two:
+                        wizard_embed.title = "Edit Event Time & Date"
+                        wizard_embed.description = "Your next message will be the new time and date for the event."
+                        await wizard_message.edit(embed=wizard_embed)
+                        try:
+                            user_message = await self.client.wait_for("message", timeout=60, check=check)
+                        except asyncio.TimeoutError:
+                            pass
+                        else:
+                            try:
+                                new_event.datetime = strptime(user_message.content)
+                                break
+                            except ValueError:
+                                await ctx.send("Invalid time format.", delete_after=3)
+                    elif reaction.emoji == _three:
+                        wizard_embed.title = "Edit Event Description"
+                        wizard_embed.description = "Your next message will be the new description for the event."
+                        await wizard_message.edit(embed=wizard_embed)
+                        try:
+                            user_message = await self.client.wait_for("message", timeout=60, check=check)
+                        except asyncio.TimeoutError:
+                            pass
+                        else:
+                            new_event.description = user_message.content
+                            break
+                    elif reaction.emoji == _cancel:
+                        return
+
+        title = "Change Event Details?"
+        description = "Are you sure you want to change the **Old** event details into the **New** ones?"
+
+        result: DialogReturn = await yes_no(ctx, title, description, [
+            ("Old:",
+             f"**Name:**\n> {old_event.name}\n**Description:**\n```\n{old_event.description}\n```\n**Time:**\n> {asctime(old_event.datetime)}"),
+            ("New:",
+             f"**Name:**\n> {new_event.name}\n**Description:**\n```\n{new_event.description}\n```\n**Time:**\n> {asctime(new_event.datetime)}")
+        ], timeout=60.0)
+
+        if result in (DialogReturn.NO, DialogReturn.FAILED, DialogReturn.ERROR):
+            config.events[index - 1] = old_event
+
+        self.save_configs(ctx.guild.id)
+
+    # endregion
+
+    # region Triggers
+    @commands.group(name="Trigger", invoke_without_command=True)
+    async def trigger(self, ctx: commands.Context, index: int = None):
+        config: EventConfig = self.guildDict[ctx.guild.id]
+        embed: Embed = Embed()
+        if index:
+            if index - 1 >= len(config.event_reminder_triggers):
+                raise commands.BadArgument(f"Index {index} out of range.")
+
+            trigger: EventReminderTrigger = config.event_reminder_triggers[index - 1]
+
+            embed.title = f"Index *{index}*"
+            embed.add_field(name="Hours Difference", value=f"> {trigger.hour_diff}")
+            embed.add_field(name="Minutes Difference", value=f"> {trigger.minute_diff}")
+            embed.add_field(name="Message", value=f"```\n{trigger.message}\n```", inline=False)
+        else:
+            embed.title = "List of Triggers."
+            for trigger in config.event_reminder_triggers:
+                embed.add_field(name=f"Index {config.event_reminder_triggers.index(trigger) + 1}",
+                                value=f"> **Hours Diff:** {trigger.hour_diff}, **Minutes Diff:** {trigger.minute_diff}",
+                                inline=False)
+
+        await ctx.send(embed=embed)
+
+    @trigger.command(name="add")
+    async def add_trigger(self, ctx: commands.Context, hour_dif: int, minute_dif: int, *, message: str):
+        self.guildDict[ctx.guild.id].event_reminder_triggers.append(EventReminderTrigger(hour_dif, minute_dif, message))
+        self.save_configs(ctx.guild.id)
+
+    @trigger.command(name="edit")
+    async def edit_trigger(self, ctx: commands.Context, index: int, hour_diff: int, minute_diff: int, *, message: str):
+        config: EventConfig = self.guildDict[ctx.guild.id]
+
+        if index - 1 >= len(config.event_reminder_triggers) or index <= 0:
+            raise commands.BadArgument(f"Index {index} is not a valid index.")
+
+        old_trigger: EventReminderTrigger = copy.deepcopy(new_trigger := config.event_reminder_triggers[index - 1])
+
+        new_trigger.hour_diff = hour_diff
+        new_trigger.minute_diff = minute_diff
+        new_trigger.message = message
+
+        values = [
+            ("Old:",
+             f"```\n {old_trigger.message} \n```\n> **Hours Diff**: {old_trigger.hour_diff}, **Minutes Diff**: {old_trigger.minute_diff}"),
+            ("New:",
+             f"```\n {new_trigger.message} \n```\n> **Hours Diff**: {new_trigger.hour_diff}, **Minutes Diff**: {new_trigger.minute_diff}")
+        ]
+
+        result: DialogReturn = await yes_no(ctx, "Are you sure?",
+                                            "Are you sure you want to change the trigger from **Old** to **New**?",
+                                            values)
+
+        if result in (DialogReturn.NO, DialogReturn.FAILED, DialogReturn.ERROR):
+            config.event_reminder_triggers[index - 1] = old_trigger
+
+        self.save_configs(ctx.guild.id)
+
+    @trigger.command(name="remove")
+    async def remove_trigger(self, ctx: commands.Context, index: int, confirm: bool = False):
+        config: EventConfig = self.guildDict[ctx.guild.id]
+
+        if index - 1 >= len(config.event_reminder_triggers) or index < 0:
+            raise commands.BadArgument(f"Index {index} is not a valid index.")
+
+        if confirm:
+            config.event_reminder_triggers.pop(index - 1)
+            self.save_configs(ctx.guild.id)
+
+        else:
+            result: DialogReturn = await yes_no(ctx, "Are you sure?",
+                                                f"Are you sure you want to remove the Trigger {index}?", timeout=20.0)
+            if result == DialogReturn.YES:
+                config.event_reminder_triggers.pop(index - 1)
+                self.save_configs(ctx.guild.id)
+
+        await ctx.send("Done.")
+
+    # endregion
 
     @property
     def get_configs(self) -> ConfigurationDictionary:
