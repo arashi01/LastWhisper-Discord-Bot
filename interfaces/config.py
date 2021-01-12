8165066183f68adeeaa5a9aee1195c7232bfa5ee
alex <alexandruturcanu915@gmail.com>
2021-01-12 18:08:48 +0300

    Added Cog as one of the base classes to Interfaces. (so that if you implement an interface you are a cog.)

--- interfaces/config.py
+++ interfaces/config.py
@@ -1,35 +1,37 @@
-from abc import ABC, ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
+from interfaces import CogABCMeta
 
 from configuration import ConfigurationDictionary
+from discord.ext.commands import Cog
 
 
-class _HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing(ABC, metaclass=ABCMeta):
+class _HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing(ABC, metaclass=CogABCMeta):
     def __init__(self):
         self._guildDict: dict = {}
 
 
-class ILoader(_HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, metaclass=ABCMeta):
+class ILoader(_HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, Cog, metaclass=CogABCMeta):
     @abstractmethod
     def load_configs(self, guild_id: int = None) -> None:
         pass
 
 
-class ISaver(_HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, metaclass=ABCMeta):
+class ISaver(_HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, Cog, metaclass=CogABCMeta):
     @abstractmethod
     def save_configs(self, guild_id: int = None) -> None:
         pass
 
 
-class IConfigFileManager(ILoader, ISaver, ABC, metaclass=ABCMeta):
+class IConfigFileManager(ILoader, ISaver, _HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, metaclass=CogABCMeta):
     pass
 
 
-class IConfigDeliverer(_HiddenGuildDictObjToEnsureThatSelfGuildDictIsAThing, ABC, metaclass=ABCMeta):
+class IConfigDeliverer(ABC, Cog, metaclass=CogABCMeta):
     @property
     @abstractmethod
     def get_configs(self) -> ConfigurationDictionary:
         pass
 
 
-class IConfigManager(ILoader, ISaver, IConfigDeliverer, ABC, metaclass=ABCMeta):
+class IConfigManager(IConfigFileManager, IConfigDeliverer, ABC, metaclass=CogABCMeta):
     pass
