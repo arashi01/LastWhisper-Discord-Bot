e9fe87d444aae1e00c0bcb17913b3d491e75cd11
alex <alexandruturcanu915@gmail.com>
2021-01-25 23:18:24 +0300

    Added new config.

--- objects/cog_configs/_general_config.py
+++ objects/cog_configs/_general_config.py
@@ -7,7 +7,8 @@ class GeneralConfig(CustomConfigObject):
     def __init__(self, should_clear_command: bool = False,
                  clear_command_exception_list: TypeList = None,
                  management_role_ids: TypeList = None,
-                 prefix: str = "|"):
+                 prefix: str = "|",
+                 change_prefix: list = None):
         """
         :param should_clear_command: Bool check if any command executed should be deleted.
         :param clear_command_exception_list: A list of Discord member ids that acts as a override to the should_clear_command.
@@ -19,3 +20,5 @@ class GeneralConfig(CustomConfigObject):
         self.clear_command_exception_list: TypeList = TypeList(TypeObjects.Member) if not clear_command_exception_list else clear_command_exception_list
         self.management_role_ids: TypeList = TypeList(TypeObjects.Role) if not management_role_ids else management_role_ids
         self.prefix: str = prefix
+
+        self.change_prefix: list = change_prefix or []
