b5bea5367cd9714d32fafc3acd6fbc4a6466b7eb
alex <alexandruturcanu915@gmail.com>
2021-01-25 23:23:43 +0300

    Renamed to reply.

--- cogs/extensions/management_tools.py
+++ cogs/extensions/management_tools.py
@@ -15,21 +15,21 @@ class ManagementTools(CogClass, name=utils.CogNames.ManagementTools.value):
 
     @commands.command()
     async def clear(self, ctx: commands.Context, number: str = "3"):
-        guild: ManagementToolsConfig = self._guildDict[ctx.guild.id]
+        guild: ManagementToolsConfig = self.guildDict[ctx.guild.id]
 
         if guild.clear_channel_id_blacklist.__contains__(ctx.channel.id):
-            await ctx.send("Sorry this channel has been blacklisted from this command.", delete_after=5)
+            await ctx.reply("Sorry this channel has been blacklisted from this command.", delete_after=5)
             return
 
         if str.isnumeric(number):
             if int(number) == 0:
-                await ctx.send("Really... what were you expecting to be cleared with 0.", delete_after=5)
+                await ctx.reply("Really... what were you expecting to be cleared with 0.", delete_after=5)
             else:
                 await ctx.channel.purge(limit=int(number))
         elif str(number).lower() == "all":
-            await ctx.channel.purge(limit=len(await ctx.channel.history(limit=None).flatten()))
+            await ctx.channel.purge(limit=None)
         else:
-            await ctx.send(f"Cannot remove {number} as it is not a valid command.", delete_after=5)
+            await ctx.reply(f"Cannot remove {number} as it is not a valid command.", delete_after=5, mention_author=False)
 
         await ctx.send("Finished clearing.", delete_after=5)
 
