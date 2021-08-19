ac6d146ac8a46f83bf12201757ec3022acdd9b74
alex <alexandruturcanu915@gmail.com>
2021-08-19 23:30:21 +0300

    began work on the config manager(will be renamed)

--- bot/extensions/configExtension.py
+++ bot/extensions/configExtension.py
@@ -1,13 +1,18 @@
 from discord.ext.commands import Bot, Cog, command, Context
+from utils import saveLoad
 
 
 class Config(Cog):
     def __init__(self, bot: Bot):
-        self.bot = bot
+        self._bot = bot
 
-    @command()
-    async def test(self, ctx: Context):
-        await ctx.send("Whats APP")
+        flag, obj = saveLoad.load_from_json("configs.json")
+        self._configs: dict[str, dict] = obj if flag else {}
+
+    def cog_unload(self):
+        saveLoad.save_to_json("configs.json", self._configs)
+
+    # todo: set config, load config, etc.
 
 
 def setup(bot: Bot):
