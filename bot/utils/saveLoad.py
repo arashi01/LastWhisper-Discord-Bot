a1fce31c53055ac3073cf530b08d284ea0e85110
alex <alexandruturcanu915@gmail.com>
2021-08-22 17:06:36 +0300

    Added support or "Complex" objects to be serialized into json.

--- bot/utils/saveLoad.py
+++ bot/utils/saveLoad.py
@@ -3,10 +3,19 @@ import json
 from pathlib import Path
 
 
-def save_to_json(file_path: str, obj: dict):
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, o):
+        if hasattr(o, "to_json"):
+            print(o.to_json())
+            return o.to_json()
+        else:
+            return json.JSONEncoder.default(self, o)
+
+
+def save_to_json(file_path: str, obj):
     path: Path = Path(file_path)
     with open(path, "w") as file:
-        json.dump(obj, file, indent=2)
+        json.dump(obj, file, indent=4, sort_keys=True, cls=ComplexEncoder)
 
 
 def load_from_json(file_path: str) -> (bool, dict):
