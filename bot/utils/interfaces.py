056bcdbea879c86fa678a0851d376f445dbab2b7
alex <alexandruturcanu915@gmail.com>
2021-08-24 00:09:30 +0300

    made a json serializable class thing

--- bot/utils/interfaces.py
+++ bot/utils/interfaces.py
@@ -1,2 +1,10 @@
-class ExtensionConfig:
-    pass
+class JsonSerializable:
+    @property
+    def to_json(self) -> dict:
+        return self.__dict__
+
+    @classmethod
+    def from_json(cls, data: dict):
+        obj: cls = cls()
+        obj.__dict__ = data
+        return obj
